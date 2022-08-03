5c85f961ac17c28c30cb725b30eee74ffb11fde1
darmon17 <heriwinong01@gmail.com>
2022-08-03 12:04:29 +0700

    fix get role  profile user

--- feature/users/delivery/response.go
+++ feature/users/delivery/response.go
@@ -1,10 +1,11 @@
 package delivery
 
 type User struct {
-	ID       int
-	Name     string
-	Email    string
-	Password string
-	Phone    string
-	Address  string
+	id       int
+	name     string
+	email    string
+	password string
+	phone    string
+	address  string
+	role     string
 }
